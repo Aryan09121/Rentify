6585a5b6dfe7fee996487192efe6b3bbef461ba1
Aryan9901 <911aaryan@gmail.com>
2024-04-29 17:01:35 +0530

    file location changes inside redux folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/app.scss";
 import { Provider } from "react-redux";
-import { store } from "./store.js";
+import { store } from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
