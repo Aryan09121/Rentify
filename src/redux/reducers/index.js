bc8446d6afb1dfdbc821778755f858dd2890f0e7
Aryan9901 <911aaryan@gmail.com>
2024-04-29 17:04:18 +0530

    making index in actions and reducer to reduce imports

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { carReducer } from "./car.reducer";
+import { invoiceReducer } from "./invoice.reducer";
+import { ownerReducer } from "./owner.reducer";
+import { tripReducer } from "./trip.reducer";
+import { userReducer } from "./user.reducer";
+
+export { carReducer, invoiceReducer, tripReducer, userReducer, ownerReducer };
---
+++ src/redux/reducers/invoice.reducer.js
@@ -0,0 +1,36 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const invoiceReducer = createReducer(initialState, (builder) => {
+	builder
+		.addCase("GENERATE_SINGLE_INVOICE_REQUEST", (state) => {
+			state.loading = true;
+		})
+		.addCase("GENERATE_SINGLE_INVOICE_SUCCESS", (state, action) => {
+			state.loading = false;
+			state.message = action.payload.message;
+		})
+		.addCase("GENERATE_SINGLE_INVOICE_FAILURE", (state, action) => {
+			state.loading = false;
+			state.error = action.payload;
+		})
+		.addCase("GET_ALL_INVOICES_REQUEST", (state) => {
+			state.loading = true;
+		})
+		.addCase("GET_ALL_INVOICES_SUCCESS", (state, action) => {
+			state.loading = false;
+			state.invoices = action.payload.invoices;
+			// state.message = action.payload.message;
+		})
+		.addCase("GET_ALL_INVOICES_FAILURE", (state, action) => {
+			state.loading = false;
+			// state.error = action.payload;
+		})
+		.addCase("CLEAR_ERRORS", (state) => {
+			state.error = null;
+		})
+		.addCase("CLEAR_MESSAGES", (state) => {
+			state.message = null;
+		});
+});
