0a08c017142df4fedcced400c9c7f1aa2ea5dc46
Aryan9901 <911aaryan@gmail.com>
2024-04-29 17:04:30 +0530

    invoice pdf generation

--- src/components/BillPdf.jsx
+++ src/components/BillPdf.jsx
@@ -1,19 +1,39 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line react/prop-types
 /* eslint-disable react-hooks/exhaustive-deps */
 import generatePDF, { usePDF, Resolution, Margin } from "react-to-pdf";
 import AdminSidebar from "./AdminSidebar";
 import BillTableHOC from "./BillTableHOC";
 import { useCallback, useEffect, useState } from "react";
-import { invoice as invoicedata } from "../assets/data/bill";
-import { useLocation, useParams } from "react-router-dom";
+import { invoice, invoice as invoicedata } from "../assets/data/bill";
+import { useLocation } from "react-router-dom";
+
+function formatDate(date) {
+	// Ensure date is in the correct format
+	if (!(date instanceof Date)) {
+		date = new Date(date);
+	}
+
+	// Array of month names
+	const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+	// Get components of the date
+	const year = date.getFullYear();
+	const month = date.getMonth();
+	const day = date.getDate();
+
+	// Format the date
+	const formattedDate = `${day}, ${months[month]}, ${year}`;
+
+	return formattedDate;
+}
 
 const option = {
 	filename: "Invoice.pdf",
-	method: "save",
+	method: "open",
 	resolution: Resolution.MEDIUM,
 	page: {
-		margin: Margin.MEDIUM,
-		orientation: "portrait",
+		margin: Margin.SMALL,
 	},
 };
 
@@ -46,65 +66,118 @@ const findInvoiceByBillId = (billId) => {
 	return null;
 };
 
-const gst = 18;
+const gst = 5;
 
 function BillPdf() {
 	const { targetRef } = usePDF();
-	const location = useLocation();
-	const [owner, setOwner] = useState({});
 	const [bills, setBills] = useState([]);
-	const [invoice, setInvoice] = useState({});
-	const { id } = useParams();
-	const billType = location.state && location.state.billType;
+	const [invoices, setInvoices] = useState([]);
+	const [invdata, setInvdata] = useState([]);
+	const [total, setTotal] = useState([]);
+	const [owner, setOwner] = useState();
+	const location = useLocation();
+	const searchParams = new URLSearchParams(location.search);
+	const encodedInvoices = searchParams.get("invoices");
+	const encodedOwner = searchParams.get("owner");
 
 	const generate = () => {
 		generatePDF(targetRef, option);
 	};
 
-	const Table = useCallback(BillTableHOC(columns, bills, "dashboard-product-box", "Bill Details", false));
+	useEffect(() => {
+		// Decode the encoded invoices and parse them back to an array
+		if (encodedInvoices.length > 0) {
+			const decodedInvoices = encodedInvoices ? JSON.parse(decodeURIComponent(encodedInvoices)) : [];
+			setInvdata(decodedInvoices);
+		}
+		if (encodedOwner) {
+			const decodedOwner = encodedOwner ? JSON.parse(decodeURIComponent(encodedOwner)) : null;
+			setOwner(decodedOwner);
+		}
+	}, [encodedInvoices, encodedOwner]);
 
 	useEffect(() => {
-		if (billType !== "individual") {
-			const invoice = invoicedata.find((invoice) => invoice._id === id);
-			const billsdata = invoice.bills.map((bill) => ({
-				item: bill.description,
-				days: bill.days,
-				rate: bill.rate,
-				amount: bill.amount,
-			}));
-			const inv = {
-				_id: invoice._id,
-				invoiceno: invoice._id,
-				amount: invoice.amount,
-				issuedate: invoice.issuedate,
-				status: invoice.status,
-				gstinno: invoice.gstin,
-			};
-			setInvoice(inv);
-			setOwner(invoice.owner);
-			setBills(billsdata);
-		} else {
-			const invoice = findInvoiceByBillId(id);
-			const bill = invoice.bills.find((bill) => bill._id === id);
-			setOwner(invoice.owner);
-			setBills([
-				{
-					item: bill.description,
-					days: bill.days,
-					rate: bill.rate,
-					amount: bill.amount,
-				},
-			]);
-			setInvoice({
-				_id: invoice._id,
-				invoiceno: invoice._id,
-				amount: bill.amount,
-				issuedate: invoice.issuedate,
-				status: invoice.status,
-				gstinno: invoice.gstin,
+		if (invdata.length > 0) {
+			const length = invdata?.length;
+			const data = invdata.map((inv) => {
+				// console.log(inv);
+				// Calculate dayQty - offroad and then sum of all dayQty
+				const totalDayQty = inv.invoice.reduce((total, current) => {
+					return total + (current.dayQty - current.offroad);
+				}, 0);
+
+				// Sum of all kmQty
+				const totalKmQty = inv.invoice.reduce((total, current) => {
+					return total + current.kmQty;
+				}, 0);
+
+				// Sum of all dayAmount
+				const totalDayAmount = inv.invoice.reduce((total, current) => {
+					return total + current.dayAmount;
+				}, 0);
+
+				// Sum of all kmAmount
+				const totalKmAmount = inv.invoice.reduce((total, current) => {
+					return total + current.kmAmount;
+				}, 0);
+
+				// Find the earliest and latest dates
+				const fromDate = inv.invoice.reduce((earliest, current) => {
+					return earliest < new Date(current.from) ? earliest : new Date(current.from);
+				}, new Date(inv.invoice[0].from));
+
+				const toDate = inv.invoice.reduce((latest, current) => {
+					return latest > new Date(current.to) ? latest : new Date(current.to);
+				}, new Date(inv.invoice[0].to));
+
+				// Format the dates
+				const formattedFromDate = formatDate(fromDate);
+				const formattedToDate = formatDate(toDate);
+				return {
+					model: inv?.model,
+					count: inv?.invoice?.length,
+					periodFrom: formattedFromDate,
+					periodTo: formattedToDate,
+					totalDayQty: totalDayQty,
+					totalKmQty: totalKmQty,
+					totalDayAmount: totalDayAmount,
+					totalKmAmount: totalKmAmount,
+					dayRate: inv?.invoice[0].dayRate,
+					kmRate: inv?.invoice[0].kmRate,
+					length: length,
+				};
+			});
+			setInvoices(data);
+		}
+	}, [invdata]);
+
+	useEffect(() => {
+		if (invoices.length > 0) {
+			// Calculate the sum of totalDayAmount and totalKmAmount for each invoice
+			const subtotals = invoices.map((invoice) => {
+				return invoice.totalDayAmount + invoice.totalKmAmount;
+			});
+
+			// Calculate the total of subtotals
+			const totalAmount = subtotals.reduce((total, amount) => {
+				return total + amount;
+			}, 0);
+
+			// Calculate GST (5%)
+			const gstAmount = totalAmount * (gst / 100);
+
+			// Calculate the bill total
+			const billTotal = totalAmount + gstAmount;
+
+			// Set the total state
+			setTotal({
+				subtotals: subtotals,
+				totalAmount: totalAmount,
+				gstAmount: gstAmount,
+				billTotal: billTotal,
 			});
 		}
-	}, []);
+	}, [invoices]);
 
 	return (
 		<div className="admin-container">
@@ -114,72 +187,110 @@ function BillPdf() {
 					Download PDF
 				</button>
 				<div className="pdf" ref={targetRef}>
-					<header>
-						<div className="companyDetails">
-							<img src="/public/travels.png" alt="company logo" />
-							<div className="details">
-								<h3 className="regular">CRM Dashboard</h3>
-								<h4 className="regular">Street Address, City, Country</h4>
-								<h4 className="regular">462042</h4>
+					<header className="header"></header>
+					<main className="pdfBody">
+						<h2>Tax Invoice</h2>
+						<div className="basicDetails">
+							<div>
+								<p>GST NO : {owner?.gst}</p>
+								<p>HSN Services : {owner?.hsn}</p>
+								<p>Pan Card No : {owner?.pan}</p>
+							</div>
+							<div>
+								<p>Invoice NO : {owner?.gst}</p>
+								<p>Date : </p>
 							</div>
 						</div>
-						<div className="invoiceDetails">
-							<h3 className="regular">
-								Invoice# <span>{invoice.invoiceno}</span>
-							</h3>
-							<h3 className="regular">Issue Date</h3>
-							<h3 className="regular">{invoice.issuedate}</h3>
-						</div>
-					</header>
-					<main>
-						<h2>INVOICE DETAILS</h2>
-						<h3 className="gstin">
-							GSTIN# <span>{invoice.gstinno}</span>
-						</h3>
-						<p className="regular">
-							Here are your invoice details. Thank you for choosing our vehicle tracking system for your rental cars.
-						</p>
-						<div className="ownerDetails">
-							<h2>Bill To:</h2>
-							<div className="details">
-								<h3>
-									Name: <span>{owner.name}</span>
-								</h3>
-								<h3>
-									Email: <span>{owner.email}</span>
-								</h3>
-								<h3>
-									Phone Number: <span>{owner.phone}</span>
-								</h3>
-								<h3>
-									Address: <span>{owner.address}</span>
-								</h3>
+						<div className="inviceContainer">
+							<div className="invoiceHeaders">
+								<div>
+									<h3>Bill to Address:</h3>
+									<h4>
+										{owner?.address?.street +
+											", " +
+											owner?.address?.city +
+											",\n" +
+											owner?.address?.state +
+											", India, " +
+											owner?.address?.pincode}
+									</h4>
+								</div>
+								<div>
+									<p>Month : JAN-2024</p>
+									<p>Vehicle Monthly Rental Basis PO No -</p>
+								</div>
 							</div>
-						</div>
-						<div className="bills">{Table()}</div>
-						<div className="billTotal">
-							<h2>Bill Total</h2>
-							<div className="subtotal">
-								<h3>Sub total</h3>
-								<h5>₹ {bills.reduce((total, bill) => total + parseFloat(bill.amount), 0)}</h5>
+							<div className="invoiceBody">
+								{invoices?.map((invoice, idx) => {
+									return (
+										<div key={invoice.from + idx}>
+											<div>
+												<p>{idx + 1}</p>
+												<article>
+													Rental (Hiring Charges) - {invoice.model}
+													<br />
+													Description: Hiring Charges for {invoice.model} Vehicles on per day basis as per Annexure - 1
+													<br />
+													Vehicle Count - {invoice.count} Nos
+													<br />
+													Period - {invoice.periodFrom} To {invoice.periodTo}
+												</article>
+											</div>
+											<div>
+												<p>{invoice.totalDayQty}</p>
+												<p>Days</p>
+												<p>{invoice.dayRate}</p>
+												<p>{invoice.totalDayAmount}</p>
+											</div>
+										</div>
+									);
+								})}
+								{invoices?.map((invoice, idx) => {
+									return (
+										<div key={invoice.from + idx}>
+											<div>
+												<p>{invoice?.length + idx + 1}</p>
+												<article>
+													Minor Maint Charges - {invoice.model}
+													<br />
+													Description: Minor maintainance charges for {invoice.model} Vehicles on actual per KM reading
+													basis
+													<br />
+													Vehicle Count - {invoice.count} Nos
+													<br />
+													Period - {invoice.periodFrom} To {invoice.periodTo}
+												</article>
+											</div>
+											<div>
+												<p>{invoice.totalKmQty}</p>
+												<p>KM</p>
+												<p>{invoice.kmRate}</p>
+												<p>{invoice.totalKmAmount}</p>
+											</div>
+										</div>
+									);
+								})}
 							</div>
-							<div className="gst">
-								<h3>GST</h3>
-								<h5>₹ {parseFloat(bills.reduce((total, bill) => total + parseFloat(bill.amount), 0) * (gst / 100)).toFixed(2)}</h5>
-							</div>
-							<div className="total">
-								<h3>Total Amount</h3>
-								<h5>
-									₹
-									{(
-										parseFloat(bills.reduce((total, bill) => total + parseFloat(bill.amount), 0)) * (gst / 100) +
-										parseFloat(bills.reduce((total, bill) => total + parseFloat(bill.amount), 0))
-									).toFixed(2)}
-								</h5>
+							<div className="invoiceFooter">
+								<div>
+									<p>Taxable Value (Rs): </p>
+									{/* <p>2882549.64</p> */}
+									<p>{total?.totalAmount}</p>
+								</div>
+								<div>
+									<p>GST 5%: </p>
+									{/* <p>144127.48</p> */}
+									<p>{total?.gstAmount}</p>
+								</div>
+								<div>
+									<p>Total (Rs): </p>
+									{/* <p>3026677.12</p> */}
+									<p>{total?.billTotal}</p>
+								</div>
 							</div>
 						</div>
 					</main>
-					<footer className="footer">©️ Copyright &nbsp; Reserved with</footer>
+					<footer className="footer"></footer>
 				</div>
 			</main>
 		</div>
